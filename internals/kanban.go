cf36c9e2053bdd450a51ced2a4fdd2aac575ac84
ArshhGill <arshh.gill@outlook.com>
2024-02-09 17:40:51 +0530

    add structs and data.json

--- internals/kanban.go
+++ internals/kanban.go
@@ -4,6 +4,24 @@ import (
 	"fmt"
 )
 
+type Card struct{
+    Title string
+    Description string
+    Tags []string
+    Status int
+}
+
+type Board struct{
+    Name string
+    Cards []Card
+}
+
+type Project struct{
+    Name string
+    Boards []Board
+    Created_on int
+}
+
 func Start(args []string) {
 	switch args[0] {
 
