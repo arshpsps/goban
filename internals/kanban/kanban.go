dacf81f4faa19f346d1e0a636fdde33e6b1f8f83
ArshhGill <arshh.gill@outlook.com>
2024-03-07 00:38:37 +0530

    change to fmt.Errorf()

--- internals/kanban/kanban.go
+++ internals/kanban/kanban.go
@@ -11,7 +11,8 @@ func jsonRead(data string) (JsonData, error) {
 	var jsonData JsonData
 	if !json.Valid([]byte(data)) {
 		fmt.Println("invalid JSON:", data)
-		return *new(JsonData), errors.New(`invalid JSON:, ${data}`)
+		// return *new(JsonData), errors.New(`invalid JSON:, ${data}`)
+        return *new(JsonData), fmt.Errorf("invalid JSON:, %s", data)
 	}
 	json.Unmarshal([]byte(data), &jsonData)
 	fmt.Println(jsonData.Projects[0].Boards[0].Cards[0].Tags[0])
