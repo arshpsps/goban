2a0ae123cc533054a439f81ef90364eb0f4808f1
ArshhGill <arshh.gill@outlook.com>
2024-03-07 01:39:38 +0530

    bbtl github example

--- internals/bblt/bbtl.go
+++ internals/bblt/bbtl.go
@@ -1,23 +1,77 @@
 package bblt
 
 import (
-	"github.com/charmbracelet/bubbles/list"
+	// "github.com/charmbracelet/bubbles/list"
+	"fmt"
+	"os"
+
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-type model struct{
-    choices []string
-    cursor int
-    selected map[int]struct{}
+type model struct {
+	choices  []string
+	cursor   int
+	selected int
 }
 
 func initialModel() model {
-    return model {
-        choices: []string{"item1", "item2"},
-        selected: make(map[int]struct{}),
-    }
+	return model{
+		choices:  []string{"item1", "item2", "uwu", "uwuwuwuwuwu"},
+		selected: 0,
+	}
 }
 
-func (m model) init() tea.Cmd {
-    return nil
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+		case "down", "j":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+		case "enter", " ":
+			m.selected = m.cursor
+		}
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+	s := "Which option to select, uwu?\n\n\n"
+	for i, choice := range m.choices {
+		cursor := " "
+		if m.cursor == i {
+			cursor = ">"
+		}
+
+		selected := " "
+		if m.selected == i {
+			selected = "x"
+		}
+
+		s += fmt.Sprintf("%s [%s] %s\n", cursor, selected, choice)
+	}
+
+	s += "\nPress q to quit.\n"
+
+	return s
+}
+
+func Run() {
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("err, you fucked up: %v", err)
+		os.Exit(1)
+	}
 }
