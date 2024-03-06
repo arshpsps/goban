b91e65072702101f29bd0b2a5c7236324b72a57d
ArshhGill <arshh.gill@outlook.com>
2024-03-07 01:39:50 +0530

    make main work with bubbletea

--- cmd/main.go
+++ cmd/main.go
@@ -1,25 +1,26 @@
 package main
 
 import (
-    "os"
-    "log"
-    "goban/internals/kanban"
-    // tea "github.com/charmbracelet/bubbletea"
+	"goban/internals/bblt"
+	// "goban/internals/kanban"
+	// "log"
+	// "os"
+	// tea "github.com/charmbracelet/bubbletea"
 )
 
-type model struct{
-
-}
-
 func main() {
-    if len(os.Args) < 2 {
-        log.Fatalf("insufficient args: %v", os.Args)
-    }
-
-    // file, err := os.OpenFile("../data/data.json", os.O_RDWR, 0777)
-    // if err != nil {
-    //     log.Fatalf("error opening file: %s", err)
-    // }
-
-    kanban.Start(os.Args[1:])
+	bblt.Run()
 }
+
+// func pain() {
+// 	if len(os.Args) < 2 {
+// 		log.Fatalf("insufficient args: %v", os.Args)
+// 	}
+//
+// 	// file, err := os.OpenFile("../data/data.json", os.O_RDWR, 0777)
+// 	// if err != nil {
+// 	//     log.Fatalf("error opening file: %s", err)
+// 	// }
+//
+// 	kanban.Start(os.Args[1:])
+// }
