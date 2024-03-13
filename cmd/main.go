ff5943bb3cc7204f6df1110dbd96d681039a0783
ArshhGill <arshh.gill@outlook.com>
2024-03-13 12:00:38 +0530

    call bubbletea's Run()

--- cmd/main.go
+++ cmd/main.go
@@ -1,26 +1,9 @@
 package main
 
 import (
-	// "goban/internals/bblt"
-	"goban/internals/kanban"
-	"log"
-	"os"
-	// tea "github.com/charmbracelet/bubbletea"
+	"goban/internals/bblt"
 )
 
-// func main() {
-// 	bblt.Run()
-// }
-
 func main() {
-	if len(os.Args) < 2 {
-		log.Fatalf("insufficient args: %v", os.Args)
-	}
-
-	// file, err := os.OpenFile("../data/data.json", os.O_RDWR, 0777)
-	// if err != nil {
-	//     log.Fatalf("error opening file: %s", err)
-	// }
-
-	kanban.Start(os.Args[1:])
+	bblt.Run()
 }
